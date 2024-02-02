f1d28c8a89fe85050c8eb54e3e80c5fdc8a6b420
MaheshNakka <mahesh.nakka@rhdhv.com>
2024-02-02 19:05:47 +0530

    Did couple of things, lol

--- Mauidible/Player.xaml.cs
+++ Mauidible/Player.xaml.cs
@@ -7,19 +7,28 @@ namespace Mauidible;
 [QueryProperty("Item", "Item")]
 public partial class Player : ContentPage
 {
-	public AudioBook Item
-	{
-		get => (AudioBook)BindingContext;
-		set => BindingContext = value;
-	}
+    public AudioBook Item
+    {
+        get => (AudioBook)BindingContext;
+        set => BindingContext = value;
+    }
 
-	AppDbContext dbContext;
+    AppDbContext dbContext;
 
-	public Player(AppDbContext appDbContext)
-	{
-		InitializeComponent();
-		dbContext = appDbContext;
-	}
+    private int CurrentChapter = 0;
+
+    public Player (AppDbContext appDbContext)
+    {
+        InitializeComponent();
+        dbContext = appDbContext;
+
+        if (Item?.ImageUrl != null)
+        {
+            var url = Item.ImageUrl!;
+
+            coverImage.Source = url.Contains("http") ? ImageSource.FromUri(new Uri(url)) : ImageSource.FromFile(url);
+        }
+    }
 
     protected override void OnNavigatedTo (NavigatedToEventArgs args)
     {
@@ -27,15 +36,16 @@ public partial class Player : ContentPage
 
         var book = Item;
 
-        if(book != null)
+        if (book != null)
         {
             Title = book.Title;
 
-            if(book.Chapters.Count > 0)
+            if (book.Chapters.Count > 0)
             {
                 MediaElement.Stop();
                 MediaElement.Source = FileMediaSource.FromFile(book.Chapters[0].Path);
                 MediaElement.Play();
+                CurrentChapter = 0;
             }
         }
     }
@@ -47,8 +57,37 @@ public partial class Player : ContentPage
             return;
         }
 
-        MediaElement.Stop();
-        MediaElement.Source = FileMediaSource.FromFile(item.Path);
-		MediaElement.Play();
+        var chapterNumber = Item.Chapters.FindIndex(0, x => x.Id == item.Id); ;
+
+        if (chapterNumber > 0)
+        {
+            CurrentChapter = chapterNumber;
+        }
+
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            MediaElement.Stop();
+            MediaElement.Source = FileMediaSource.FromFile(item.Path);
+            MediaElement.Play();
+        });
+    }
+
+    private void MediaElement_MediaEnded (object sender, EventArgs e)
+    {
+        CurrentChapter++;
+
+        var chapter = Item.Chapters[CurrentChapter];
+
+        MainThread.BeginInvokeOnMainThread(() =>
+        {
+            MediaElement.Source = FileMediaSource.FromFile(chapter.Path);
+            MediaElement.Stop();
+            MediaElement.Play();
+        });
+    }
+
+    private void ContentPage_Unloaded (object sender, EventArgs e)
+    {
+        MediaElement.Handler?.DisconnectHandler();
     }
 }

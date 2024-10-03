bb8fa037bfff87f5b03eef7b6b99deebbfae640d
Mahesh Nakka <m.nakka@devon.nl>
2024-10-03 11:52:33 +0530

    PR-1-Updated a lot of things

--- Mauidible/MainPage.xaml.cs
+++ Mauidible/MainPage.xaml.cs
@@ -6,7 +6,7 @@ using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Storage;
 
 using Mauidible.Domain;
-
+using Mauidible.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Mauidible
@@ -14,36 +14,34 @@ namespace Mauidible
     public partial class MainPage : ContentPage
     {
         private readonly AppDbContext _context;
+        private readonly IMetadataService metadataService;
         public ObservableCollection<AudioBook> Books { get; set; }
-        public MainPage (AppDbContext context)
+        public MainPage(AppDbContext context, IMetadataService metadata)
         {
             InitializeComponent();
             context ??= new AppDbContext();
             _context = context;
+            metadataService = metadata;
             Books = [];
 
             try
             {
-                //AddBooks();
-
                 LoadBooks();
             }
             catch (Exception ex)
             {
-                var b = ex.Message;
+                _ = Shell.Current.DisplayAlert("Error", ex.Message, "Close").ConfigureAwait(false);
             }
 
             BindingContext = this;
         }
 
-        private async void ButtonAddNewBook_Clicked (object sender, EventArgs e)
+        private async void ButtonAddNewBook_Clicked(object sender, EventArgs e)
         {
-#pragma warning disable CA1416 // Validate platform compatibility
             var result = await FolderPicker.Default.PickAsync(default);
-#pragma warning restore CA1416 // Validate platform compatibility
-            
-            List<string> audioExtensions = [".mp3", ".m4a", ".m4b"];
-            List<string> imageExtensions = [".jpg", ".png"];
+
+            List<string> audioExtensions = [.. ExtensionsList.AudioExtensions];
+            List<string> imageExtensions = [.. ExtensionsList.ImageExtensions];
 
             if (result.IsSuccessful)
             {
@@ -57,46 +55,51 @@ namespace Mauidible
                     return;
                 }
 
-                var book = new AudioBook()
-                {
-                    Title = result.Folder.Name,
-                    CreatedDate = DateTime.Now
-                };
+                var firstFile = allFiles[0];
+                var imagePath = folderFiles.FirstOrDefault(x => imageExtensions.Contains(Path.GetExtension(x).ToLower()));
 
-                var imagePath = folderFiles.Where(x => imageExtensions.Contains(Path.GetExtension(x).ToLower())).FirstOrDefault() ?? "https://th.bing.com/th/id/OIP.kvtzPKo4EgFMoA63sF0fuQHaEc";
+                var book = await metadataService.FillMetadataAndGetBook(new FileInfo(firstFile), imagePath);
 
                 book.ImageUrl = imagePath;
 #if WINDOWS
-                var chapters = allFiles.Select(x => new Chapter
+                var chapters = allFiles.ConvertAll(x => new Chapter
                 {
                     Name = Path.GetFileNameWithoutExtension(x),
                     Path = x,
-                }).ToList();
+                    Book = book
+                });
+
+                chapters = [.. chapters.OrderBy(x => x.Name)];
 
                 book.Chapters = [.. chapters];
 #endif
-//#if ANDROID
+                //#if ANDROID
 #if ANDROID
-                var tempChapters = allFiles.Select(x => new
+                var tempChapters = allFiles.ConvertAll(x => new
                 {
                     Path = x,
                     Name = x.Split('/').LastOrDefault() ?? string.Empty,
 
-                }).ToList();
+                });
 
                 try
                 {
-                    var chapters = tempChapters.Select(x => new Chapter
+                    var chapters = tempChapters.ConvertAll(x => new Chapter
                     {
                         Name = x.Name.Split('.').FirstOrDefault(),
                         Path = x.Path,
-                    }).ToList();
-                    book.Chapters = [.. chapters];
+                        Book = book
+                    });
 
+                    chapters = [..chapters.OrderBy(x => x.Name)];
+
+                    book.Chapters = [.. chapters];
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
-                    var x = ex.Message; 
+                    var x = ex.Message;
+                    await Toast.Make("Error in adding book!", ToastDuration.Short).Show();
+                    return;
                 }
 #endif
 
@@ -113,9 +116,11 @@ namespace Mauidible
             }
         }
 
-        private void LoadBooks ()
+        private void LoadBooks()
         {
             var books = _context.AudioBooks.Include(x => x.Chapters).AsNoTracking().ToList();
+
+            books = [.. books.OrderByDescending(x => x.LastUpdatedDate)];
 
             MainThread.BeginInvokeOnMainThread(() =>
             {
@@ -125,10 +130,30 @@ namespace Mauidible
                     book.ImageUrl ??= "https://th.bing.com/th/id/OIP.kvtzPKo4EgFMoA63sF0fuQHaEc";
                     Books.Add(book);
                 });
-            }); 
+
+                if (DeviceInfo.Platform == DevicePlatform.Android || DeviceInfo.Platform == DevicePlatform.iOS)
+                {
+                    // Hide the navigation bar on mobile platforms
+                    mainLayout.MaximumWidthRequest = (DeviceDisplay.Current.MainDisplayInfo.Width / DeviceDisplay.Current.MainDisplayInfo.Density) - 54;
+
+                    foreach (var book in books)
+                    {
+                        book.ImageParentMaxHeightRequest = 250;
+                        book.ImageParentMaxWidthRequest = 100;
+                        book.ImageParentMinHeightRequest = 180;
+
+                        book.ImageMaxWidthRequest = 100;
+                        book.ImageMaxHeightRequest = 180;
+                    }
+                }
+                else
+                {
+                    // mnGrid.MaximumWidthRequest = DeviceDisplay.Current.MainDisplayInfo.Width / DeviceDisplay.Current.MainDisplayInfo.Density;
+                }
+            });
         }
 
-        private async void PlayBook_Clicked (System.Object sender, System.EventArgs e)
+        private async void PlayBook_Clicked(System.Object sender, System.EventArgs e)
         {
             var button = sender as ImageButton;
 
---
+++ Mauidible/Services/IMetadataService.cs
@@ -0,0 +1,60 @@
+﻿using ATL;
+using Mauidible.Domain;
+
+namespace Mauidible.Services;
+
+public interface IMetadataService
+{
+    Task<AudioBook> FillMetadataAndGetBook(FileInfo fileInfo, string? imagePath);
+}
+
+public class MetadataService : IMetadataService
+{
+    public async Task<AudioBook> FillMetadataAndGetBook(FileInfo fileInfo, string? imagePath)
+    {
+        Track track = new(fileInfo.FullName);
+
+        string bookName = (track.Album, track.Title) switch
+        {
+            (var album, _) when !string.IsNullOrEmpty(album) => album,
+            (_, var title) when !string.IsNullOrEmpty(title) => title,
+            _ => fileInfo.Name
+        };
+
+        var book = new AudioBook()
+        {
+            Title = bookName,
+            Author = track.Artist,
+            Description = track.Comment,
+            LastUpdatedDate = DateTime.Now,
+            ImageUrl = imagePath,
+        };
+
+        if (book.ImageUrl is null or { Length: 0 })
+        {
+            var pictures = track.EmbeddedPictures;
+
+            if (pictures.Count > 0)
+            {
+                string? imageExtension = pictures[0].MimeType switch
+                {
+                    "image/jpeg" => ".jpg",
+                    "image/png" => ".png",
+                    _ => null // Default case if MIME type doesn't match known types
+                };
+
+                if (!string.IsNullOrEmpty(imageExtension))
+                {
+                    string imageFileName = $"{bookName}{imageExtension}";
+                    string imageFilePath = Path.Combine(fileInfo.Directory!.FullName, imageFileName);
+
+                    await File.WriteAllBytesAsync(imageFilePath, pictures[0].PictureData);
+
+                    book.ImageUrl = imageFilePath;
+                }
+            }
+        }
+
+        return book;
+    }
+}
